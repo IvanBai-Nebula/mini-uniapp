87081424cd54af4ac153fe3b4a69166d6ac71a94
Lenovo <luoguangsheng03@126.com>
2024-11-04 10:56:36 +0800

    增加了护理页面的图片和首页的图片，以及部分功能

--- src/pages/tab/nurse/nurse.vue
+++ src/pages/tab/nurse/nurse.vue
@@ -1,67 +1,96 @@
 <template>
-  <view class="page-wrap">
-    <up-navbar title="" placeholder left-icon="" right-icon="camera-fill" />
-    <view class="flex items-center bg-white pb-30rpx pl-30rpx pr-20rpx">
-      <view class="mr-10rpx">
-        <up-avatar src="/static/images/logo.png" size="70" />
-      </view>
-      <view class="flex-1">
-        <view class="pb-20rpx font-size-36rpx">
-          uni-app
-        </view>
-        <view class="u-tips-color font-size-28rpx" @click="toCopy">
-          微信号:uni-app
-        </view>
-      </view>
-      <view class="ml-10rpx p-10rpx">
-        <up-icon name="scan" color="#969799" />
-      </view>
-      <view class="ml-10rpx p-10rpx">
-        <up-icon name="arrow-right" color="#969799" />
-      </view>
-    </view>
-    <!--
-        //<view class="mt-20rpx bg-white">   //支付用不到
-        ////  <up-cell-group>
-        ////    <up-cell icon="rmb-circle" title="支付" is-link />
-        //// </up-cell-group>
-        ////</view>
-    -->
+  <div id="app" class="container">
+    <div class="header">
+      <img src="/static/images/nurse/house.png" alt="house icon" class="house-icon" />
+      <h1>健康自评</h1>
+    </div>
 
-    <view class="mt-20rpx bg-white">
-      <up-cell-group>
-        <up-cell icon="star" title="收藏" is-link />
-        <up-cell icon="photo" title="相册" is-link />
-        <up-cell icon="coupon" title="卡券" is-link />
-        <up-cell icon="heart" title="关注" is-link />
-      </up-cell-group>
-    </view>
+    <div class="grid-container">
+      <div class="grid-item">
+        <img src="/static/images/nurse/psychology.png" alt="心理评估" class="icon" />
+        <p>心理评估量表</p>
+      </div>
+      <div class="grid-item">
+        <img src="/static/images/nurse/sleep.png" alt="睡眠评估" class="icon" />
+        <p>睡眠评估量表</p>
+      </div>
+    </div>
 
-    <view class="mt-20rpx bg-white">
-      <up-cell-group>
-        <up-cell icon="setting" title="设置" is-link />
-      </up-cell-group>
-    </view>
-  </view>
+    <div class="bottom-container">
+      <div class="bottom-item">
+        <img src="/static/images/nurse/relaxation.png" alt="自我放松" />
+        <p>自我放松</p>
+      </div>
+      <div class="bottom-item">
+        <img src="/static/images/nurse/fitness.png" alt="强身健体" />
+        <p>强身健体</p>
+      </div>
+      <div class="bottom-item">
+        <img src="/static/images/nurse/self-healing.png" alt="自我疗愈" />
+        <p>自我疗愈</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-import { useClipboard } from '@/hooks'
-
-const { setClipboardData, getClipboardData } = useClipboard()
-
-// const userStore = useUserStore()
-// onShow(() => {
-//   if (!userStore.user) {
-//     uni.navigateTo({
-//       url: '/pages/login/index',
-//     })
-//   }
-// })
-// 复制
-const toCopy = async () => {
-  await setClipboardData({ data: '1234567890' })
-  const data = await getClipboardData()
-  console.log('[ data ] >', data)
+<script>
+export default {
+  name: 'HealthAssessment',
 }
 </script>
+
+<style scoped>
+.container {
+  text-align: center;
+  background-color: #e8f2f8;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.header {
+  background-color: #f8d2e1;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.house-icon {
+  width: 50px;
+  height: auto;
+}
+
+.grid-container {
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+}
+
+.grid-item {
+  background-color: #fff;
+  border: 2px solid #ff6f61;
+  border-radius: 5px;
+  padding: 15px;
+  margin: 0 10px;
+  width: 120px;
+}
+
+.icon {
+  width: 40px;
+  height: auto;
+}
+
+.bottom-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.bottom-item {
+  text-align: center;
+  margin: 0 15px;
+}
+
+.bottom-item img {
+  width: 40px;
+  height: auto;
+}
+</style>
